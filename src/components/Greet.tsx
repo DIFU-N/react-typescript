70d1406a6b55d92c9b67551d2ef9dfd3b836f6d3
DIFU-N <chukwudifunsofor@gmail.com>
2023-05-31 13:56:01 +0100

    Optional Prop Settings

--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,16 +1,19 @@
 //use types when building applications and interfaces when building libraries
 type GreetProps = {
   name: string,
-  messageCount: number,
+  // optional props settings ? infront of defined prop
+  messageCount?: number,
   isLoggedIn: boolean,
 }
 
 const Greet = (props: GreetProps) => {
+  const {messageCount = 0 } = props;
+  // if messageCount is passed in use that value, else use 0
   return (
     <div>
       <h2>
         {props.isLoggedIn ? 
-        `Welcome ${props.name}!! Ive been drinking more alcohol for the past ${props.messageCount} days`
+        `Welcome ${props.name}!! Ive been drinking more alcohol for the past ${messageCount} days`
         : 'Welcome, but I do not know that man'}
       </h2>
     </div>
