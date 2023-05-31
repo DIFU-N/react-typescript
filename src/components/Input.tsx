23395b30ad614c2f5292a551e4ef84e78dad8c3b
DIFU-N <chukwudifunsofor@gmail.com>
2023-05-31 16:42:07 +0100

    destructuring comp props

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,18 +1,31 @@
-import React from "react"
+import React from "react";
 
 type InputProps = {
-    value: string,
-    handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void
-}
+  value: string;
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
 
-export const Input = (props: InputProps) => {
-    //this keeps the eventchange fxn the same, whether it's passed in or not
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        console.log(event);
-    }
+//it is also possible to use Props by destructuring the InputProps obj as so:
+export const Input = ({value, handleChange}: InputProps) => {
+  //this keeps the eventchange fxn the same, whether it's passed in or not
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event);
+  };
   return (
     <div>
-        <input type="text" value={props.value} onChange={handleInputChange}/>
+      <input type="text" value={value} onChange={handleChange} />
     </div>
-  )
-}
+  );
+};
+
+// export const Input = (props: InputProps) => {
+//     //this keeps the eventchange fxn the same, whether it's passed in or not
+//     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//         console.log(event);
+//     }
+//   return (
+//     <div>
+//         <input type="text" value={props.value} onChange={handleInputChange}/>
+//     </div>
+//   )
+// }
