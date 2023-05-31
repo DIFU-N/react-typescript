886d1ca203c9842ab548194c8e5a27d81c165619
DIFU-N <chukwudifunsofor@gmail.com>
2023-05-31 12:57:21 +0100

    more props types. arrays of objects {}[]

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import Greet from "./components/Greet"
+import Person from "./components/Person"
+import PersonList from "./components/PersonList"
 
 function App() {
+  const personName = {
+    first: 'Bruce',
+    last: 'Banner'
+  }
+  const nameList = [
+    {
+      first: 'Bruce',
+      last: 'Banner'
+    },
+    {
+      first: 'Stiles',
+      last: 'Stilinski'
+    },
+    {
+      first: 'Mike',
+      last: 'Ross'
+    },
+  ]
   return (
     <div>
       <Greet name='People' messageCount={5} isLoggedIn={true} />
+      <Person name={personName} />
+      <PersonList names={nameList} />
     </div>
   )
 }
