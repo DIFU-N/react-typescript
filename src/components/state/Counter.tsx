e59614ec490c68980942901b622674000bfaf885
DIFU-N <chukwudifunsofor@gmail.com>
2023-06-01 11:29:12 +0100

    useReducer strict action

--- src/components/state/Counter.tsx
+++ src/components/state/Counter.tsx
@@ -4,10 +4,24 @@ type CounterState = {
     count: number,
 }
 
-type CounterAction = {
-    type: string,
-    payload: number,
+// this is known as discriminated unions and it is the preferred way to call useReducer actions
+type UpdateAction = {
+    //strict action types
+    type: 'increment' | 'decrement'
+    payload: number
 }
+// this is known as discriminated unions and it is the preferred way to call useReducer actions
+type ResetAction = {
+    //strict action types
+    type: 'reset'
+}
+
+// this is known as discriminated unions and it is the preferred way to call useReducer actions
+// A discriminated type union is where you use code flow
+// analysis to reduce a set of potential objects down to one
+// specific object.
+//this is the preferred way to define strict types with useReducer
+type CounterAction = UpdateAction | ResetAction
 
 const initialState = { count: 0 }
 
@@ -17,6 +31,8 @@ function reducer(state: CounterState, action: CounterAction) {
       return { count: state.count + action.payload };
     case "decrement":
       return { count: state.count - action.payload };
+    case "reset":
+        return initialState;
     default:
       return state;
   }
@@ -29,6 +45,9 @@ export const Counter = () => {
     Count: {state.count}
     <button onClick={() => dispatch({type: 'increment', payload: 10})}>
         Increment 10
+    </button>
+    <button onClick={() => dispatch({type: 'reset'})}>
+        Reset
     </button>
     <button onClick={() => dispatch({type: 'decrement', payload: 10})}>
         Decrement 10
